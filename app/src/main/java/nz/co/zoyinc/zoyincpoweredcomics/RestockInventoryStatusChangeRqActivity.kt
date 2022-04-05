ffebe787c64c063da6aa5c5896ee7c17c2e28e62
JoshuaPaterson15 <joshua.paterson@student.hhs.school.nz>
2022-04-06 10:02:51 +1200

    Updated: Added and transfered RestockInventoryStatusChangeRqActivity.kt and /.xml
    Added a secondary background with a stroke.
    
    Added new string values alongside the transfer of activity_restock_inventory_status_change_rq.xml

---
+++ app/src/main/java/nz/co/zoyinc/zoyincpoweredcomics/RestockInventoryStatusChangeRqActivity.kt
@@ -0,0 +1,11 @@
+package nz.co.zoyinc.zoyincpoweredcomics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RestockInventoryStatusChangeRqActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restock_inventory_status_change_rq)
+    }
+}
